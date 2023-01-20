200a769dc65a7f5f0cb95e954e58fa3fa18ce78f
Terioch <riostockton@gmail.com>
2023-01-20 18:56:39 +0000

    Revert

--- SimpleBlog/ClientApp/components/shared/navigation.js
+++ SimpleBlog/ClientApp/components/shared/navigation.js
@@ -4,42 +4,14 @@ class Navigation {
   }
 
   onInit() {
-    console.log("OnInit");
     document
       .getElementById("postSearchForm")
       .addEventListener("submit", this.searchPosts.bind(this));
-
-    document
-      .getElementById("registerForm")
-      .addEventListener("submit", this.register.bind(this));
   }
 
   async searchPosts(e) {
     e.preventDefault();
     window.location.href = `/postSearch.php?search=${e.target["searchInput"].value}`;
-  }
-
-  async register(e) {
-    e.preventDefault();
-    console.log(e.target);
-    const userName = e.target["registerUserNameInput"].value;
-    const email = e.target["registerEmailInput"].value;
-    const password = e.target["registerPasswordInput"].value;
-
-    const userDto = {
-      userName,
-      email,
-      password,
-    };
-    console.log(userDto);
-    const response = await fetch(`https://localhost:7133/api/auth/register`, {
-      method: "POST",
-      body: JSON.stringify(userDto),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    const result = await response.json();
-    console.log(result);
   }
 }
 
